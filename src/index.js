620c4b90eb094890fddc9609b3d3ff5467f94338
JSJ1377 <jeremy.stephen.jones@gmail.com>
2023-10-13 21:49:33 -0400

    added scrits for deployment
    scripts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 ///import App from './App';
 import Play2Learn from './components/containers/Play2learn'
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
